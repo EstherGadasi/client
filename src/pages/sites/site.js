af3362de039b7e631263a7ad12fdd7cebff903b7
estyandtzivi <36325201192@mby.co.il>
2023-02-28 16:35:59 +0200

    28/02/2023 16:35

--- src/pages/sites/site.js
+++ src/pages/sites/site.js
@@ -22,7 +22,7 @@ const Site = () => {
          const fetchData=async()=> {
             let config = {
                 headers: {
-                  'Authorization': 'Bearer '// + localStorage.getItem("token")
+                  'Authorization': 'Bearer ' + localStorage.getItem("token")
                 }
             }
             const site = await axios.get("http://localhost:3000/site")
@@ -38,105 +38,14 @@ const Site = () => {
       }, []);
 
 
-      const handleAddBook = async (e) => {
-        e.preventDefault();
-        try {  
-            let config = {
-                headers: {
-                  'Authorization': 'Bearer ' + localStorage.getItem("token")
-                }
-            }    
-          const res = await axios.post("http://localhost:3000/site",  {  idimage,num_of_turist,ages,children,discription,time_it_takes,accible,place1,place2,address}, config);
-          navigate("/book/list")
-        } catch (err) {
-          setErr(err.response.data?.message);
-        }
-      };
-    //   if(!categories.length || ! autours.length) return <h1>LOADING....</h1>
+      
   return (
     <>
     <div className="new-book">
-        <form>
+        
 
         
-    {/* <select  onChange={(e)=>set(e.target.value)} >
-        <option>-select--</option>
-        {categories.map((category)=>{
-            return <option  value={category.id} >{category.name}</option>
-        })}
-    </select>
-    <select  onChange={(e)=>setAuthour(e.target.value)}>
-    <option>-select--</option>
-
-        {autours.map((author)=>{
-            return <option value={author.id} >{author.name}</option>
-        })}
-    </select> */}
-   
-    <input
-              type="text"
-              placeholder="idimage"
-              name="idimage"
-              onChange={(e)=>setidimage(e.target.value)}
-            />
-    <input
-              type="text"
-              placeholder="num_of_turist"
-              name="num_of_turist"
-              onChange={(e)=>setnum_of_turist(e.target.value)}
-            />
-    <input
-        type="text"
-        placeholder="ages"
-        name="ages"
-        onChange={(e)=>setages(e.target.value)}
-    />
-    <input
-        type="text"
-        placeholder="children"
-        name="children"
-        onChange={(e)=>setchildren(e.target.value)}
-    />
-    <input
-        type="text"
-        placeholder="discription"
-        name="discription"
-        onChange={(e)=>setdiscription(e.target.value)}
-    />
-    <input
-        type="text"
-        placeholder="time_it_takes"
-        name="time_it_takes"
-        onChange={(e)=>settime_it_takes(e.target.value)}
-    />
-    <input
-        type="text"
-        placeholder="accible"
-        name="accible"
-        onChange={(e)=>setaccible(e.target.value)}
-    />
-     <input
-        type="text"
-        placeholder="place1"
-        name="place1"
-        onChange={(e)=>setplace1(e.target.value)}
-    />
-    <input
-        type="text"
-        placeholder="place2"
-        name="place2"
-        onChange={(e)=>setplace2(e.target.value)}
-    />
-     <input
-        type="text"
-        placeholder="address"
-        name="address"
-        onChange={(e)=>setaddress(e.target.value)}
-    />
-     
-            {/* {err && err} */}
-            <button onClick={handleAddBook}>ADD Book</button>
-    </form>
+    
     </div>
    
         
