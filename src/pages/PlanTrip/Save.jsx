a9a45546ad7b0a316c65c7592037c48725fac055
estyandtzivi <36325201192@mby.co.il>
2023-03-15 09:08:51 +0200

    2

--- src/pages/PlanTrip/Save.jsx
+++ src/pages/PlanTrip/Save.jsx
@@ -3,26 +3,9 @@ import React , { useEffect, useState } from "react";
 
 import axios from "axios";
 
-<<<<<<< HEAD
+
  
-async function Save({trip,setTripid}) {
 
-
-console.log("LK")
-    // const current = new Date();
-    // const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
-    // try {
-    //     const trip = { // area:arrcs.area,
-    //         userId: 1,
-    //         begin_point1: begin_point1,
-    //         begin_point2: begin_point2,
-    //         end_point1: end_point1,
-    //         end_point2: end_point2,
-    //         date: date,
-    //         listofsites: selectOption,
-    //         constrainsoftrip: constrains
-    //     }
-=======
 
 async function Save({setTrip,selectOption,constrains, begin_point1, begin_point2,end_point1,end_point2}) {
 
@@ -40,21 +23,19 @@ async function Save({setTrip,selectOption,constrains, begin_point1, begin_point2
             listofsites: selectOption,
             constrainsoftrip: constrains
         }
->>>>>>> 6f756724b2a6bbfc4e6c5d6166d5b08ed178abeb
+
+
 
 
         const res = await axios.post("http://localhost:4000/trip", { trip });//the url not excat
 
-<<<<<<< HEAD
-        setTripid(res.data.tripid)
-=======
-        setTrip(res.data.tripid)
->>>>>>> 6f756724b2a6bbfc4e6c5d6166d5b08ed178abeb
+       setTripid(res.data.tripid)
+
+}
 
        
        
     // } catch (err) {
     //     // setErr(err.response.data?.message);
     // }
-}
-export default Save;
+ } export default Save;
